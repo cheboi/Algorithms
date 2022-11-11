8dc7314dd4fe19b9f84e6415ec4551764efbd87d
cheboi <moscheb96@gmail.com>
2022-11-11 22:02:41 +0300

    binary search

---
+++ arrays/search.cs
@@ -0,0 +1,25 @@
+using system;
+
+public static object BinarySearchRecursive(int[] inputArray, int key, int min, int max)
+{
+    if (min > max)
+    {
+        return "Nil";
+    }
+    else
+    {
+        int mid = (min + max) / 2;
+        if (key == inputArray[mid])
+        {
+            return ++mid;
+        }
+        else if (key < inputArray[mid])
+        {
+            return BinarySearchRecursive(inputArray, key, min, mid - 1);
+        }
+        else
+        {
+            return BinarySearchRecursive(inputArray, key, mid + 1, max);
+        }
+    }
+}
